b5f6593935bd26ffc144042912960cf1dc5d0c5a
jmc000 <martincocher.jeremy@gmail.com>
2026-07-21 17:13:50 -0500

    export profiler file on run

--- src/bitcoin_indexer/__main__.py
+++ src/bitcoin_indexer/__main__.py
@@ -2,6 +2,10 @@ import logger
 import rpc as rpc
 import db as db
 
+#todo
+import cProfile
+import pstats
+
 from sqlalchemy.engine import Engine
 
 logger = logger.setup_logging(__name__)
@@ -17,7 +21,11 @@ if __name__ == "__main__":
     # -----------
     #  MVP
     # -----------
-    block_number = 957354
-    engine = db.set_up_db()
-    insert_block_with_txs(block_number, engine)
-
+    with cProfile.Profile() as profile:
+        block_number = 957361
+        engine = db.set_up_db()
+        insert_block_with_txs(block_number, engine)
+    
+    results = pstats.Stats(profile)
+    results.sort_stats(pstats.SortKey.TIME)
+    results.dump_stats("./var/results.prof")
